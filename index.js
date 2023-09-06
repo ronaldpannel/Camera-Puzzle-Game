14af67dd3362121b67634f1a2d7efe02193c78a1
ronaldpannel <69686310+ronaldpannel@users.noreply.github.com>
2023-09-06 08:29:30 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 /**@type{HTMLCanvasElement} */
 
-
 let VIDEO = null;
 let CANVAS = null;
 let CONTEXT = null;
+let SCALER = 0.8;
+let SIZE = { x: 0, y: 0, width: 0, height: 0 };
 
 function main() {
   CANVAS = document.getElementById("canvas1");
   CONTEXT = CANVAS.getContext("2d");
-  CANVAS.width = window.innerWidth
-  CANVAS.height = window.innerHeight
+
   let promise = navigator.mediaDevices.getUserMedia({ video: true });
   promise
     .then(function (signal) {
@@ -18,15 +18,32 @@ function main() {
       VIDEO.play();
 
       VIDEO.onloadeddata = function () {
-        updateCanvas()
+        handleResize();
+        window.addEventListener("resize", handleResize);
+
+        updateCanvas();
       };
     })
     .catch(function (err) {
       alert("camera  error; " + err);
     });
 }
-function updateCanvas(){
-  CONTEXT.drawImage(VIDEO,0,0,CANVAS.width, CANVAS.height)
-    window.requestAnimationFrame(updateCanvas);
-}
 
+function handleResize() {
+  CANVAS.width = window.innerWidth;
+  CANVAS.height = window.innerHeight;
+  let resizer =
+    SCALER *
+    Math.min(
+      window.innerWidth / VIDEO.videoWidth,
+      window.innerHeight / VIDEO.videoHeight
+    );
+  SIZE.width = resizer * VIDEO.videoWidth;
+  SIZE.height = resizer * VIDEO.videoHeight;
+  SIZE.x = window.innerWidth / 2 - SIZE.width / 2;
+  SIZE.y = window.innerHeight / 2 - SIZE.height / 2;
+}
+function updateCanvas() {
+  CONTEXT.drawImage(VIDEO, SIZE.x, SIZE.y, SIZE.width, SIZE.height);
+  window.requestAnimationFrame(updateCanvas);
+}
